21f0768a4d3be0206320f629aa50c8d49cdb9ba0
Kevin Lee <leekevin@berkeley.edu>
2015-05-28 13:54:03 -0700

    keys

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
     $(".background").css("height", $(window).height()+"px");
-    $("#subtitle").css("display", "none");
     $("#task1").css("height", $(window).height()+"px");
+    $("#task2").css("height", $(window).height()+"px");
 });
