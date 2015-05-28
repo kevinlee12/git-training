141c8bd592a97ac02cc8a2f462f62114ec48d91b
Kevin Lee <leekevin@berkeley.edu>
2015-05-28 13:38:52 -0700

    files for git practice

---
+++ main.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $(".background").css("height", $(window).height()+"px");
+    $("#subtitle").css("display", "none");
+    $("#task1").css("height", $(window).height()+"px");
+});
