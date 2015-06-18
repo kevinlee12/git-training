d2d7835ad48c32f3fbdd632b54130cd44f291d1f
Janice <janicez348@gmail.com>
2015-06-17 23:00:03 -0700

    do task1

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
     $(".background").css("height", $(window).height()+"px");
-    $("#subtitle").css("display", "none");
     $("#task1").css("height", $(window).height()+"px");
 });
