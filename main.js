f7cc62119d427113851ce5f84fec79527b32b2d9
Nathan Lopez-Gates <nlopezgates@berkeley.edu>
2016-02-24 17:29:49 -0800

    changed task1 to height 100px

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 $(document).ready(function () {
     $(".background").css("height", $(window).height()+"px");
-    $("#task1").css("height", $(window).height()+"px");
+    $("#task1").css("height", "100px");
 });
